f8bb99c012b9903eea5465d3e0e129ace9d1db7d
rbhagat <rbhagat@mythics.com>
2022-11-02 14:48:36 -0400

    Updating the home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,16 +16,16 @@ const Header = () => {
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/about">
-              <Nav.Link>About Us</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-              <Nav.Link>Contact Us</Nav.Link>
-          </LinkContainer>
+          <Nav className="me-auto justify-content-end">
+            <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+                <Nav.Link>About Us</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+                <Nav.Link>Contact Us</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,19 +12,44 @@ const  Home = () => {
         <Header />
         <div style={{background: "url(https://mythics.com/wp-content/uploads/2022/07/AdobeStock_245269525-scaled.jpeg)"}} class="jumbo-image bg-cover text-white">
             <div class="container py-5 text-center">
-                <strong><h1 class="display-4 font-weight-bold">Mythics Emergent Group</h1></strong>
+                {/* <strong><h1 class="display-4 font-weight-bold">Mythics Emergent Group</h1></strong> */}
             </div>
         </div>
 
         <div class="container py-5">
-            <h2 class="h3 font-weight-bold">Cloud Solutions Provider</h2>
             <div class="row">
-                <div class="col-lg-12 mb-4">
-                    <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
-                    <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                <div className='col-4'>
+                    <Card >
+                        <Card.Img variant="top" src="mythics.png" />
+                        <Card.Body>
+                            <Card.Title>DevOps Solution</Card.Title>
+                            <Card.Text>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <div class="col-lg-12">
-                    <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur <strong class="font-weight-bold text-dark">adipisicing elit, sed </strong>do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <strong class="font-weight-bold text-dark">reprehenderit in voluptate </strong>velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className='col-4'>
+                    <Card >
+                        <Card.Img variant="top" src="mythics.png" />
+                        <Card.Body>
+                            <Card.Title>Cloud Consulting</Card.Title>
+                            <Card.Text>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className='col-4'>
+                    <Card >
+                        <Card.Img variant="top" src="mythics.png" />
+                        <Card.Body>
+                            <Card.Title>Infrastrucutre Patching</Card.Title>
+                            <Card.Text>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </div>
