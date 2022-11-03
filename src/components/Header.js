2acc5a98f353ad1d3d15e8c044b1ceca12229273
rbhagat <rbhagat@mythics.com>
2022-11-03 08:39:08 -0400

    Added sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
 import {LinkContainer} from 'react-router-bootstrap'
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import {useState} from 'react';
+
 
 const Header = () => {
-  
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <Navbar bg="light" expand="lg">
@@ -14,9 +20,10 @@ const Header = () => {
         <LinkContainer to={"/"}>
           <Navbar.Brand href="#home">Mythics Cloud Portal</Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow} />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-end">
+        <Offcanvas show={show} onHide={handleClose} responsive="lg" backdrop="true">
+          <Nav className="me-auto">
             <LinkContainer to="/">
                 <Nav.Link>Home</Nav.Link>
             </LinkContainer>
@@ -27,7 +34,8 @@ const Header = () => {
                 <Nav.Link>Contact Us</Nav.Link>
             </LinkContainer>
           </Nav>
-        </Navbar.Collapse>
+        </Offcanvas>
+      </Navbar.Collapse>
       </Container>
     </Navbar>
   );
